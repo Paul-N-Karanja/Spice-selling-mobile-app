91b095896b394b10428db441e735d5edc47f4a4e
Paul-N-Karanja <p.ndungu.kara@gmail.com>
2024-12-14 01:18:28 +0300

    added a variable and function to enable switching between different sections of the application bottom navigation bar

--- lib/base/footer_bar.dart
+++ lib/base/footer_bar.dart
@@ -11,7 +11,23 @@ class FooterBar extends StatefulWidget {
 }
 
 class _FooterBarState extends State<FooterBar> {
+  final appScreens = [
+    const Center(child: Text("Home")),
+    const Center(child: Text("Search")),
+    const Center(child: Text("Spices")),
+    const Center(child: Text("Herbs")),
+    const Center(child: Text("Cereals")),
+    const Center(child: Text("Account")),
+  ];
 
+  // change bottom nav bar index value
+
+  int _selectedIndex=0;
+
+  void _onTapBottomNavBarItem(int index){
+    _selectedIndex=index;
+    print("index number is $_selectedIndex");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,25 +36,35 @@ class _FooterBarState extends State<FooterBar> {
         backgroundColor: Colors.lightBlueAccent,
         title: const Center(child: Text("Items in stock")),
       ),
-      body: const Center(
-        child: Text("Spice information"),
-      ),
+      body: appScreens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.grey,
+        onTap: _onTapBottomNavBarItem,
+          selectedItemColor: Colors.grey,
           unselectedItemColor: Colors.lightBlue,
           showSelectedLabels: true,
           showUnselectedLabels: true,
           items: const [
-            BottomNavigationBarItem(label: "Home",icon: Icon(Icons.home),
+            BottomNavigationBarItem(
+                label: "Home",
+                icon: Icon(Icons.home),
                 activeIcon: Icon(Icons.home_filled)),
-            BottomNavigationBarItem(label: "Search",icon: Icon(CupertinoIcons.search_circle),
-            activeIcon: Icon(CupertinoIcons.search_circle_fill)),
-            BottomNavigationBarItem(label: "Spices",icon: Icon(FontAwesomeIcons.pepperHot)),
-            BottomNavigationBarItem(label: "Herbs",icon: Icon(CupertinoIcons.leaf_arrow_circlepath)),
-            BottomNavigationBarItem(label: "Cereals",icon: Icon(Icons.grain),
-            activeIcon: Icon(Icons.grain_sharp)),
-            BottomNavigationBarItem(label: "Account",icon: Icon(CupertinoIcons.person_circle),
-            activeIcon: Icon(CupertinoIcons.person_circle_fill)),
+            BottomNavigationBarItem(
+                label: "Search",
+                icon: Icon(CupertinoIcons.search_circle),
+                activeIcon: Icon(CupertinoIcons.search_circle_fill)),
+            BottomNavigationBarItem(
+                label: "Spices", icon: Icon(FontAwesomeIcons.pepperHot)),
+            BottomNavigationBarItem(
+                label: "Herbs",
+                icon: Icon(CupertinoIcons.leaf_arrow_circlepath)),
+            BottomNavigationBarItem(
+                label: "Cereals",
+                icon: Icon(Icons.grain),
+                activeIcon: Icon(Icons.grain_sharp)),
+            BottomNavigationBarItem(
+                label: "Account",
+                icon: Icon(CupertinoIcons.person_circle),
+                activeIcon: Icon(CupertinoIcons.person_circle_fill)),
           ]),
     );
   }
